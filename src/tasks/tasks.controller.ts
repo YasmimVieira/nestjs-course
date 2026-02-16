deec58f2bf0a715cde126659df757983472147c2
YasmimVieira <vieiray937@gmail.com>
2026-02-16 17:10:51 -0300

    feat: get just one param

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
@@ -6,6 +6,11 @@ export class TasksController {
     @Get()
     public findAll(): string[] {
         return ['A', 'B']
+    }
+
+    @Get('/:id')
+    public findOne(@Param() params: any): string {
+        return params
     }
 }
 
