208f7f415222061ff0886c258471ca4e937e970a
YasmimVieira <vieiray937@gmail.com>
2026-02-16 17:09:23 -0300

    feat: add get all elements

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageFormatterService } from './services/message-formatter/message-formatter.service';
 import { LoggerService } from './services/logger/logger.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService, MessageFormatterService, LoggerService],
 })
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+
+    @Get()
+    public findAll(): string[] {
+        return ['A', 'B']
+    }
+}
+
+
